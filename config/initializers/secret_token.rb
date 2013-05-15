596f1bd53571532f0f7fdabd8da1a9e8969ccbbf
lbs <lbspen@gmail.com>
2013-05-14 23:59:15 -0700

    Initial generation of the app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DateTracker::Application.config.secret_token = 'b40ed22f960516a88f398ec40a795a26029d29454b522fcf000de8c4f9d17c3701eed8c2e1d7b6798fcee34b5ce3a59bbb1e4abbd3ada671b162a92bd42c584a'
